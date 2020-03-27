019b5e16f19185d947e91aa636b7df867c85ba89
Ben Haus <29528517+phillybenh@users.noreply.github.com>
2020-03-27 10:52:09 -0400

    Update form_tests.js

--- user-onboarding-form/cypress/integration/form_tests.js
+++ user-onboarding-form/cypress/integration/form_tests.js
@@ -6,22 +6,22 @@ describe("Test form inputs and submittal", function () {
         cy.visit("http://localhost:3000/");
     });
 
-    // it("Add tests for form inputs and submit button", function () {
-    //     cy.get('input[name="name"]')
-    //         .type("Jean-Luc Picard")
-    //         .should("have.value", "Jean-Luc Picard");
-    //     cy.get('input[name="email"]')
-    //         .type("captPicard@galacyclassss.sft")
-    //         .should("have.value", "captPicard@galacyclassss.sft");
-    //     cy.get('input[name="pwd"]')
-    //         .type("NCC-1701-D")
-    //         .should("have.value", "NCC-1701-D");
-    //     cy.get('[type="checkbox"]')
-    //         .check()
-    //         .should("be.checked");
-    //     cy.get('button')
-    //         .click()
-    // });
+    it("Add tests for form inputs and submit button", function () {
+        cy.get('input[name="name"]')
+            .type("Jean-Luc Picard")
+            .should("have.value", "Jean-Luc Picard");
+        cy.get('input[name="email"]')
+            .type("captPicard@galacyclassss.sft")
+            .should("have.value", "captPicard@galacyclassss.sft");
+        cy.get('input[name="pwd"]')
+            .type("NCC-1701-D")
+            .should("have.value", "NCC-1701-D");
+        cy.get('[type="checkbox"]')
+            .check()
+            .should("be.checked");
+        cy.get('button')
+            .click()
+    });
 
     it("Check form validation if input is left empty", function () {
         cy.get('input:invalid').should('have.length', 0)
